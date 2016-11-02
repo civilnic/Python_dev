4a8d073e9de244f3a2dc86983c07a422063b13dc
nbonnet <nicolas.bonnet@sogeti.com>
2016-11-02 23:13:46 +0100

    MICD parser in progress
    Signed-off-by: nbonnet <nicolas.bonnet@sogeti.com>

---
+++ MICD/MICD.py
@@ -0,0 +1,46 @@
+import pathlib
+import re
+import xlrd, xlwt
+
+class MICD:
+    """
+    Class to parse/create MICD
+    """
+    xls_style=xlwt.easyxf('font: name Arial, bold off, height 200;')
+
+    file_structure = {
+        'HEADER': ('Identifier', 'Value'),
+        'SIM_CTRL_IN': ('Name', 'Type', 'Dim1', 'Unit', 'Description', 'Comment'),
+        'SIM_CTRL_OUT': ('Name', 'Type', 'Dim1', 'Unit', 'Description', 'Comment'),
+        'PROFILE': ('Name', 'Type', 'Dim1', 'Description'),
+        'AIRCRAFT_ICD': ('ICD Version', 'ICD File Name', 'Cross Ref File Name'),
+        'SIMULATION_LEVEL': ('Platform Code', 'Simulation Level[1]'),
+        'FUN_IN': ('Name', 'Type', 'Unit', 'Description', 'Convention', 'Dim1', 'Dim2', 'Com Format', 'Com Mode', 'From'
+                   , "Refresh\nRate", 'Min', 'Max', 'Enum', 'Consumed If', 'Aircraft Signal Name', "Interface\nLevel",
+                   'Status (SSM/FS/Refresh)',"Simulation\nLevel[1]", 'Init Value', 'Custom', 'Comment'),
+        'FUN_OUT': ('Name', 'Type', 'Unit', 'Description', 'Convention', 'Dim1', 'Dim2', 'Com Format', 'Com Mode',
+                    'To', "Refresh\nRate",'Min', 'Max', 'Enum', 'Produced If', 'Aircraft Signal Name',"Interface\nLevel"
+                    , 'Status (SSM/FS/Refresh)', "Simulation\nLevel[1]", 'Comment', 'Not Simulated Data',
+                    'Default Value')
+    }
+
+    def __init__(self, pathname):
+
+        self._pathName = pathname
+        self._modelName = None
+
+
+
+    def parse(self):
+        return
+
+
+    # function to create a new MICD file
+    def create(self):
+        return
+
+    # function to save current MICD object (pathname attribute
+    # is used to save the file
+    def save(self):
+        return
+
