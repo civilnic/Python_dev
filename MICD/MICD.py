430e804a561c2af9cfaf9f841f5d0df2d85f881e
nbonnet <nicolas.bonnet@sogeti.com>
2016-11-03 23:35:25 +0100

    MICD empty is OK
    Signed-off-by: nbonnet <nicolas.bonnet@sogeti.com>

--- MICD/MICD.py
+++ MICD/MICD.py
@@ -1,12 +1,14 @@
 import pathlib
 import re
+from datetime import datetime
 import xlrd, xlwt
 
 class MICD:
     """
     Class to parse/create MICD
     """
-    xls_style = xlwt.easyxf('font: name Arial, bold off, height 200;')
+    xls_style = xlwt.easyxf('font: name Arial, bold off, height 200; align: horiz left;border: top thin, right thin, bottom thin, left thin;')
+    xls_style_bold = xlwt.easyxf('font: name Arial, bold on, height 200; pattern: pattern solid, fore_color turquoise; border: top thin, right thin, bottom thin, left thin; align: horiz left;')
 
     file_structure = {
         'HEADER': ('Identifier', 'Value'),
@@ -24,10 +26,11 @@ class MICD:
                     'Default Value')
     }
 
-    def __init__(self, pathname, modelname=None, newfile=None):
+    def __init__(self, pathname, modelname=None, modelversion=None, newfile=None):
 
         self._pathName = pathname
         self._modelName = modelname
+        self._version = modelversion
         self._newFile = newfile
         self._Workbook = None
         self._SheetAndIndex = {}
@@ -56,44 +59,78 @@ class MICD:
 
         self._Workbook = xlwt.Workbook()
 
-        for sheet in self._SheetAndIndex:
+        # create file structure
+        for _sheet in self._SheetAndIndex:
 
             # add sheet in workbook
-            self._SheetAndIndex[sheet]['XlsSheet'] = self._Workbook.add_sheet(sheet)
+            self._SheetAndIndex[_sheet]['XlsSheet'] = self._Workbook.add_sheet(_sheet)
 
             # header creation
-            for header_cell in list(self._SheetAndIndex[sheet]['Header']):
-                self._SheetAndIndex[sheet]['XlsSheet'].write(self._SheetAndIndex[sheet]['RowIndex'],
-                                                             self._SheetAndIndex[sheet]['ColIndex'],
-                                                             header_cell, MICD.xls_style)
-                self._SheetAndIndex[sheet]['ColIndex'] += 1
+            for _header_cell in list(self._SheetAndIndex[_sheet]['Header']):
+
+                self._SheetAndIndex[_sheet]['XlsSheet'].write(self._SheetAndIndex[_sheet]['RowIndex'],
+                                                              self._SheetAndIndex[_sheet]['ColIndex'],
+                                                              _header_cell,
+                                                              MICD.xls_style_bold)
+                self._SheetAndIndex[_sheet]['ColIndex'] += 1
 
             # update object information
-            self._SheetAndIndex[sheet]['ColIndex'] = 0
-            self._SheetAndIndex[sheet]['RowIndex'] += 1
-            self._SheetAndIndex[sheet]['RowNbr'] += 1
+            self._SheetAndIndex[_sheet]['ColIndex'] = 0
+            self._SheetAndIndex[_sheet]['RowIndex'] += 1
+            self._SheetAndIndex[_sheet]['RowNbr'] += 1
 
+        # fill header sheet
+        _sheet = 'HEADER'
 
+        # date computation for information
+        _date = datetime.now()
+        _displaydate = str(_date.day)+"/"+str(_date.month)+"/"+str(_date.year)
 
+        # create and temp dictonnary it will be used to fill header sheet in a loop
+        _headerDict = {'MOD': self._modelName,
+                       'VER': self._version,
+                       'ORG': 'EYYSEV',
+                       'DAT': _displaydate,
+                       'ACI': '',
+                       'APP': '',
+                       'REF': '',
+                       'VIT': '8.1'}
+        # loop to write header cell
+        for _ident, _value in _headerDict.items():
 
+            self.writeCell(_sheet, 'Identifier', _ident,True)
+            self.writeCell(_sheet, 'Value', _value)
+            self._SheetAndIndex[_sheet]['RowIndex'] += 1
+            self._SheetAndIndex[_sheet]['RowNbr'] += 1
 
         return
 
-    def writeCell(self, sheet, field, value):
 
-        index = self.file_structure[sheet].index(field)
+
+    def writeCell(self,sheet,field,value,title=None):
+
+        if title is None:
+            _styleToApply = MICD.xls_style
+        elif title is True:
+            _styleToApply = MICD.xls_style_bold
+        else:
+            _styleToApply = None
+        _index = self.file_structure[sheet].index(field)
+
 
         self._SheetAndIndex[sheet]['XlsSheet'].write(self._SheetAndIndex[sheet]['RowIndex'],
-                                                    index,
-                                                    value,
-                                                    MICD.xls_style
-                                                    )
+                                                     _index,
+                                                     value,
+                                                     _styleToApply)
         self._SheetAndIndex[sheet]['ColIndex'] += 1
-
 
 
     # function to save current MICD object (pathname attribute
     # is used to save the file
-    def save(self):
-        return
+    def savefile(self):
+        """
+        Method to save a BDS2XML tool input file from BDS2XML current object
+        :return True/False:
+        """
+        self._Workbook.save(self._pathName)
 
