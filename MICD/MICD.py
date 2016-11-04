7a725796c4ba8f1a52f1d23570712b7ef4009fd8
nbonnet <civil_,nic@yahoo.fr>
2016-11-04 18:11:24 +0100

    MICD parser
    Signed-off-by: nbonnet <civil_,nic@yahoo.fr>

--- MICD/MICD.py
+++ MICD/MICD.py
@@ -1,12 +1,14 @@
 import pathlib
 import re
+from datetime import datetime
 import xlrd, xlwt
 
 class MICD:
     """
     Class to parse/create MICD
     """
-    xls_style = xlwt.easyxf('font: name Arial, bold off, height 200;')
+    xls_style = xlwt.easyxf('font: name Arial, bold off, height 200; align: horiz justified, shrink_to_fit true, wrap on;border: top thin, right thin, bottom thin, left thin;')
+    xls_style_bold = xlwt.easyxf('font: name Arial, bold on, height 200; pattern: pattern solid, fore_color turquoise; border: top thin, right thin, bottom thin, left thin; align: horiz left;')
 
     file_structure = {
         'HEADER': ('Identifier', 'Value'),
@@ -24,10 +26,11 @@ class MICD:
                     'Default Value')
     }
 
-    def __init__(self, pathname, modelname=None, newfile=None):
+    def __init__(self, pathname, modelname=None, modelversion=None, newfile=None):
 
         self._pathName = pathname
         self._modelName = modelname
+        self._version = modelversion
         self._newFile = newfile
         self._Workbook = None
         self._SheetAndIndex = {}
@@ -56,44 +59,155 @@ class MICD:
 
         self._Workbook = xlwt.Workbook()
 
-        for sheet in self._SheetAndIndex:
+        # create file structure
+        for _sheet in self._SheetAndIndex:
 
             # add sheet in workbook
-            self._SheetAndIndex[sheet]['XlsSheet'] = self._Workbook.add_sheet(sheet)
+            self._SheetAndIndex[_sheet]['XlsSheet'] = self._Workbook.add_sheet(_sheet)
 
             # header creation
-            for header_cell in list(self._SheetAndIndex[sheet]['Header']):
-                self._SheetAndIndex[sheet]['XlsSheet'].write(self._SheetAndIndex[sheet]['RowIndex'],
-                                                             self._SheetAndIndex[sheet]['ColIndex'],
-                                                             header_cell, MICD.xls_style)
-                self._SheetAndIndex[sheet]['ColIndex'] += 1
+            for _header_cell in list(self._SheetAndIndex[_sheet]['Header']):
+
+                self._SheetAndIndex[_sheet]['XlsSheet'].write(self._SheetAndIndex[_sheet]['RowIndex'],
+                                                              self._SheetAndIndex[_sheet]['ColIndex'],
+                                                              _header_cell,
+                                                              MICD.xls_style_bold)
+                self._SheetAndIndex[_sheet]['ColIndex'] += 1
 
             # update object information
-            self._SheetAndIndex[sheet]['ColIndex'] = 0
-            self._SheetAndIndex[sheet]['RowIndex'] += 1
-            self._SheetAndIndex[sheet]['RowNbr'] += 1
+            self._SheetAndIndex[_sheet]['ColIndex'] = 0
+            self._SheetAndIndex[_sheet]['RowIndex'] += 1
+            self._SheetAndIndex[_sheet]['RowNbr'] += 1
 
+        # fill header sheet
+        _sheet = 'HEADER'
 
+        # date computation for information
+        _date = datetime.now()
+        _displaydate = str(_date.day)+"/"+str(_date.month)+"/"+str(_date.year)
 
+        # create and temp dictonnary it will be used to fill header sheet in a loop
+        _headerDict = {'MOD': self._modelName,
+                       'VER': self._version,
+                       'ORG': 'EYYSEV',
+                       'DAT': _displaydate,
+                       'ACI': '',
+                       'APP': '',
+                       'REF': '',
+                       'VIT': '8.1'}
 
+        # loop to write header cell
+        for _ident, _value in _headerDict.items():
+
+            self.writeCell(_sheet, 'Identifier', _ident,True)
+            self.writeCell(_sheet, 'Value', _value)
+            self._SheetAndIndex[_sheet]['RowIndex'] += 1
+            self._SheetAndIndex[_sheet]['RowNbr'] += 1
+
+        # fill header sheet
+        _sheet = 'SIM_CTRL_IN'
+
+        # create and temp dictonnary it will be used to fill header sheet in a loop
+        _simctrlinDict = {'F_HOLD': ["boolean",
+                                     "",
+                                     "wu",
+                                     "HOLD mode input Flag",
+                                     'TRUE means HOLD mode is required by the environment.\n\
+his flag is used to implement the Freeze function or Hold function on certain \
+Simulation platforms.'],
+                          'F_INIT': ["boolean",
+                                     "",
+                                     "wu",
+                                     "INIT mode input Flag",
+                                     'TRUE means INIT mode is required by the environment.\n\
+In this mode, the model initialises all internal model data.'],
+                          'F_LOAD': ["boolean",
+                                     "",
+                                     "wu",
+                                     "LOAD mode input Flag",
+                                     'TRUE means LOAD mode is required by the environment: \
+the model is authorised to Load specific files [if needed].\n\
+In this case, the file names are to be provided in Model PROFILE Descriptor file.'],
+                          'F_REINIT': ["boolean",
+                                     "",
+                                     "wu",
+                                     "REINIT mode input Flag",
+                                     'TRUE means REINIT mode is required by the environment.\n\
+During this mode a specific behaviour is expected from the models which depends\
+on the type of platform.'],
+                          'F_RUN': ["boolean",
+                                       "",
+                                       "wu",
+                                       "RUN mode input Flag",
+                                       'TRUE means Normal Execution Simulation'],
+                          'F_UNLOAD': ["boolean",
+                                       "",
+                                       "wu",
+                                       "UNLOAD mode input Flag",
+                                       'TRUE means UNLOAD mode is required by the environment.\n\
+True means the model is required to not perform any functional computation\
+and to write on output files if required.'],
+                          'V_CETIME': ["double",
+                                       "",
+                                       "s",
+                                       "Current Environment time",
+                                       'The current absolute time in seconds of the environment this is to be used to \
+check synchronisation of the model and environment if required.'],
+                          'V_DELTAT': ["float",
+                                       "",
+                                       "s",
+                                       "Calculation step variable",
+                                       'Time between two successive model calls in second.\n\
+THIS TIME SHALL BE CONTROLLED FROM THE ENVIRONMENT.\
+The Value set at initialisation [ when F_INIT is true ] and fixed.'],
+                          'V_Sim_Id': ["char",
+                                       "256",
+                                       "wu",
+                                       "Simulator Identification",
+                                       'Simulator Platform identification set from Environment to models.\
+With this variable the Model will know which Simulator it is running on.\n\
+This variable is not refreshed in RUN mode.']
+
+                          }
+        # loop to write sim_ctrl_in cell
+        for _ident, _value in _simctrlinDict.items():
+
+            self.writeCell(_sheet, 'Name', _ident,True)
+
+            for _field in _value:
+                self.writeCell(_sheet, self.file_structure[_sheet][_value.index(_field)+1], _field)
+
+            self._SheetAndIndex[_sheet]['RowIndex'] += 1
+            self._SheetAndIndex[_sheet]['RowNbr'] += 1
 
         return
 
-    def writeCell(self, sheet, field, value):
 
-        index = self.file_structure[sheet].index(field)
+
+    def writeCell(self,sheet,field,value,title=None):
+
+        if title is None:
+            _styleToApply = MICD.xls_style
+        elif title is True:
+            _styleToApply = MICD.xls_style_bold
+        else:
+            _styleToApply = None
+        _index = self.file_structure[sheet].index(field)
+
 
         self._SheetAndIndex[sheet]['XlsSheet'].write(self._SheetAndIndex[sheet]['RowIndex'],
-                                                    index,
-                                                    value,
-                                                    MICD.xls_style
-                                                    )
+                                                     _index,
+                                                     value,
+                                                     _styleToApply)
         self._SheetAndIndex[sheet]['ColIndex'] += 1
-
 
 
     # function to save current MICD object (pathname attribute
     # is used to save the file
-    def save(self):
-        return
+    def savefile(self):
+        """
+        Method to save a BDS2XML tool input file from BDS2XML current object
+        :return True/False:
+        """
+        self._Workbook.save(self._pathName)
 
