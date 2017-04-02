39ef8c4b582ded4adf094913ae32d58f0f6eb373
nbonnet <nicolas.bonnet@sogeti.com>
2017-04-03 01:35:45 +0200

    MEX INT GUI
    Signed-off-by: nbonnet <nicolas.bonnet@sogeti.com>

--- GUI/MEX_INTG_GUI/MEX_INTG_GUI/Connexion_Wrapper.py
+++ GUI/MEX_INTG_GUI/MEX_INTG_GUI/Connexion_Wrapper.py
@@ -1,25 +1,66 @@
 from PyQt5.QtCore import QAbstractTableModel
+from PyQt5.QtCore import QVariant
+from PyQt5.QtCore import Qt
 from PyQt5 import QtCore
 import csv
 
 class ConnexionWrapper(QAbstractTableModel):
 
+
+
     def __init__(self, filename, parent=None):
         QAbstractTableModel.__init__(self, parent)
         self._thing = "cnw_wrapper"
-
+        self._objectTab = []
         self._filename = filename
 
         _csvFile = open(self._filename, "r")
 
-        self._linesList = csv.reader(_csvFile, delimiter=';')
+        #self._reader = csv.DictReader(_csvFile, delimiter=';')
+        _reader = csv.reader(_csvFile, delimiter=';')
+
+        for _line in _reader:
+            self._objectTab.append(_line)
+
+        self._headerData = self._objectTab[0]
+        print(self._headerData)
 
     def _name(self):
         return str(self._thing)
 
     def rowCount(self, parent):
-        return len(self._linesList)
+        _rows = list(self._reader)
+        return len(_rows)
 
     def columnCount(self, parent):
-        return len(self._linesList[0])
+        return len(self._headerData)
 
+    def data(self, index, role):
+        if role == QtCore.Qt.DisplayRole:
+            if not index.isValid():
+                return QVariant()
+            elif role != Qt.DisplayRole:
+                return QVariant()
+            _row = index.row()
+            _col = index.column()
+            return QVariant(self._objectTab[_row][_col])
+        return None
+
+    def headerData(self, col, orientation, role):
+        if orientation == Qt.Horizontal and role == Qt.DisplayRole:
+            return QVariant(self._headerData[col])
+        if orientation == QtCore.Qt.Vertical:
+            return ''
+
+        return None
+
+    def rowCountlt(self):
+        _rows = list(self._objectTab)
+        return len(_rows)-1
+
+
+    def columnCountlt(self):
+        return len(self._headerData)
+
+    def datalt(self,row,col):
+        return self._objectTab[row][col]
--- GUI/MEX_INTG_GUI/MEX_INTG_GUI/main.py
+++ GUI/MEX_INTG_GUI/MEX_INTG_GUI/main.py
@@ -16,23 +16,27 @@ if __name__ == '__main__':
    _file = r"F:\Mes documents\Nicolas\Boulot\Developpement\Python\Python_dev\GUI\choice_cnx.csv"
    _qml = r'F:\Mes documents\Nicolas\Boulot\Developpement\Python\Python_dev\GUI\MEX_INTG_GUI\MEX_INTG_GUI\MEX_INTG_GUI.qml'
    _Cnx_Wrapp = ConnexionWrapper(_file)
+   print(_Cnx_Wrapp.rowCountlt())
+   print(_Cnx_Wrapp.columnCountlt())
+   print(_Cnx_Wrapp.datalt(10,10))
 
    # app = QGuiApplication(sys.argv)
-   #
    # view = QQuickView()
-   # view.setResizeMode(QQuickView.SizeRootObjectToView)
-   # ctxt = view.rootContext()
-   # ctxt.setContextProperty('myModel', _Cnx_Wrapp)
    #
-   # view.setSource(QUrl.fromLocalFile('Cell.qml'))
+   # ctxt = view.rootContext()
+   # ctxt.setContextProperty('ConnexionWrapper', _Cnx_Wrapp)
+   # view.setResizeMode(QQuickView.SizeRootObjectToView)
+   # view.setSource(QUrl.fromLocalFile(_qml))
    # view.show()
    #
    # sys.exit(app.exec_())
-   #
+
+
    app = QApplication(sys.argv)
-   qmlRegisterType(ConnexionWrapper, 'Connexion', 1, 0, 'ConnexionWrapper')
+   qmlRegisterType(ConnexionWrapper, 'cnxWrapper', 1, 0, 'ConnexionWrapper')
    engine = QQmlApplicationEngine(parent=app)
    engine.load(_qml)
    win = engine.rootObjects()[0]
    win.show()
    sys.exit(app.exec_())
+
